a8ed69d27227084ddad45aee7cf698b2801bf6e2
Rene <flughoernchen1415@gmail.com>
2018-06-22 15:41:41 +0200

    initial commit

---
+++ src/main/java/reichhorn/spring/mvcrestclient/MvcrestclientApplication.java
@@ -0,0 +1,12 @@
+package reichhorn.spring.mvcrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcrestclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcrestclientApplication.class, args);
+    }
+}
